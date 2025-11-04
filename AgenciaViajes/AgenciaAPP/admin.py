8c1e8e51eed23d026947ca8874b8aa7d91a62ad1
Mikel <mikel.huete.sanchez@opendeusto.es>
2025-11-04 13:49:23 +0100

    Crear  modelos

--- AgenciaViajes/AgenciaAPP/admin.py
+++ AgenciaViajes/AgenciaAPP/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Usuario, Viaje, Actividad
 
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Viaje)
+admin.site.register(Actividad)
--- AgenciaViajes/AgenciaAPP/models.py
+++ AgenciaViajes/AgenciaAPP/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+   nombre = models.CharField(max_length=100)
+   email = models.EmailField(unique=True)
+   telefono = models.IntegerField(max_length=15, blank=True, null=True)
+   fecha_registro = models.DateField(auto_now_add=True)
+
+   def __str__(self):
+      return self.nombre
+
+
+class Viaje(models.Model):
+   destino = models.CharField(max_length=150)
+   descripcion = models.TextField()
+   fecha_inicio = models.DateField()
+   fecha_fin = models.DateField()
+   precio = models.DecimalField(max_digits=8, decimal_places=2)
+   usuario = models.ManyToManyField(Usuario, related_name='viajes')
+
+   def __str__(self):
+      return f"{self.destino} ({self.fecha_inicio} - {self.fecha_fin})"
+
+
+class Actividad(models.Model):
+   nombre = models.CharField(max_length=100)
+   descripcion = models.TextField(blank=True, null=True)
+   horario = models.TimeField(blank=True, null=True)
+   viaje = models.ManyToManyField(Viaje, related_name='actividades')
+
+   def __str__(self):
+      return f"{self.nombre} - {self.viaje.first().destino}"
