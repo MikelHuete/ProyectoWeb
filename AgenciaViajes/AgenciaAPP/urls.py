85e4a1fc1d99dbd6f4b61c3586ec0862553625aa
Mikel <mikel.huete.sanchez@opendeusto.es>
2025-11-05 13:31:28 +0100

    Creacíon de las vistas
    Creaccíon de las vistas y los HTML, a falta de realizar las mejoras

--- AgenciaViajes/AgenciaAPP/urls.py
+++ AgenciaViajes/AgenciaAPP/urls.py
@@ -4,11 +4,17 @@ from . import views
 urlpatterns = [
    path('', views.home, name='Inicio'),
 
-   # Rutas para la gestión de viajes
-   # 127.0.0.1:8000/AgenciaAPP/viajes/
    path('viajes/', views.viajes, name='Viajes'),
-
    path('actividades/', views.actividades, name='Actividades'),
-
    path('usuarios/', views.usuarios, name='Usuarios'),
+
+   # Listas y detalles
+   path('viajes/lista/', views.lista_viajes, name='lista_viajes'),
+   path('viajes/<int:pk>/', views.detalle_viaje, name='detalle_viaje'),
+
+   path('actividades/lista/', views.lista_actividades, name='lista_actividades'),
+   path('actividades/<int:pk>/', views.detalle_actividad, name='detalle_actividad'),
+
+   path('usuarios/lista/', views.lista_usuarios, name='lista_usuarios'),
+   path('usuarios/<int:pk>/', views.detalle_usuario, name='detalle_usuario'),
 ]
--- AgenciaViajes/AgenciaAPP/views.py
+++ AgenciaViajes/AgenciaAPP/views.py
@@ -1,15 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+
+from AgenciaAPP.models import Actividad, Usuario, Viaje
 
 # Create your views here.
 def home(request):
-   return HttpResponse('Inicio de la Agencia de Viajes')
+   return render(request, 'index.html')
 
 def viajes(request):
-   return HttpResponse('Página de gestión de viajes')
+   return render(request, 'viajes/index.html')
 
 def actividades(request):
-   return HttpResponse('Página de gestión de actividades')
+   return render(request, 'actividades/index.html')
 
 def usuarios(request):
-   return HttpResponse('Página de gestión de usuarios')
+   return render(request, 'usuarios/index.html')
+
+def lista_viajes(request):
+   viajes = Viaje.objects.all()
+   context = {'viajes': viajes}
+   return render(request, 'viajes/lista_viajes.html', context)
+
+def detalle_viaje(request, pk):
+   viaje = get_object_or_404(Viaje, id=pk)
+   context = {'viaje': viaje}
+   return render(request, 'viajes/detalle_viaje.html', context)
+
+def lista_actividades(request):
+   actividades = Actividad.objects.all()
+   context = {'actividades': actividades}
+   return render(request, 'actividades/lista_actividades.html', context)
+
+def detalle_actividad(request, pk):
+   actividad = get_object_or_404(Actividad, id=pk)
+   context = {'actividad': actividad}
+   return render(request, 'actividades/detalle_actividad.html', context)
+
+def lista_usuarios(request):
+   usuarios = Usuario.objects.all()
+   context = {'usuarios': usuarios}
+   return render(request, 'usuarios/lista_usuarios.html', context)
+
+def detalle_usuario(request, pk):
+   usuario = get_object_or_404(Usuario, id=pk)
+   context = {'usuario': usuario}
+   return render(request, 'usuarios/detalle_usuario.html', context)
